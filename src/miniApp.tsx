f53f8ba72908ee29927921c717659c79ee9bddf9
nikunjc24 <nikunj.bisht@cars24.com>
2025-05-21 18:15:23 +0530

    first commit

---
+++ src/miniApp.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const MiniApp = () => {
+  return (
+    <View>
+      <Text style={{fontWeight: '800', fontSize: 40, color: 'red'}}>
+        Mini App
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MiniApp;
